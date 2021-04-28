551a0090cd2c590bf42f1930e65109a9cfdb175b
merrillkoshy <merrillkoshy@gmail.com>
2021-04-28 09:32:08 +0400

    reactified

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
-module.exports = {
-  future: {
-    webpack5: true,
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+module.exports = withPlugins([
+  [
+    optimizedImages,
+    {
+      mozjpeg: {
+        quality: 80,
+      },
+      pngquant: {
+        speed: 3,
+        strip: true,
+        verbose: true,
+      },
+      imagesPublicPath: "/merrillkoshy/_next/static/images/",
+    },
+  ],
+  {
+    basePath: "/merrillkoshy",
+    assetPrefix: "/merrillkoshy/",
+    env,
   },
-  basePath: "/merrillkoshy",
-  assetPrefix: "/merrillkoshy/",
-};
+]);
