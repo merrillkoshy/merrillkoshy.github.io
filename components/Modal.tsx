36d572f9d257964b821a8a59fbb18d2b57deb631
merrillkoshy <merrillkoshy@gmail.com>
2024-05-04 15:53:34 +0800

    Update About component with revised content

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,154 +1,153 @@
-import React from "react"
-import { Details } from "../types"
-import Link from "next/link"
+import React from "react";
+import { Details } from "../types";
 
 const Modal = ({
-	show,
-	setShow,
-	title,
-	content,
-	styles,
+  show,
+  setShow,
+  title,
+  content,
+  styles,
 }: {
-	show: boolean
-	setShow: React.Dispatch<React.SetStateAction<boolean>>
-	title: string
-	content: Details
-	styles: any
+  show: boolean;
+  setShow: React.Dispatch<React.SetStateAction<boolean>>;
+  title: string;
+  content: Details;
+  styles: any;
 }) => {
-	let details: string[] = []
-	if (content.details.includes("•")) {
-		details = content.details?.split("•")
-	} else if (content.details.includes("*")) {
-		details = content.details?.split("*")
-	}
+  let details: string[] = [];
+  if (content.details.includes("•")) {
+    details = content.details?.split("•");
+  } else if (content.details.includes("*")) {
+    details = content.details?.split("*");
+  }
 
-	return (
-		<div
-			className={`modal fade ${show ? "show" : ""}`}
-			id={title.replace(/\s/g, "")}
-			tabIndex={-1}
-			role="dialog"
-			data-backdrop={show}
-			data-show={show}
-			data-focus={show}
-			aria-labelledby={title}
-			aria-hidden={`${show}`}
-			style={{
-				display: show ? "block" : "none",
-				backdropFilter: show ? "brightness(0.5)" : "none",
-				transition: "1s",
-				overflow: "scroll",
-			}}
-		>
-			<div
-				className="modal-dialog modal-dialog-centered modal-lg"
-				role="document"
-			>
-				<div
-					className="modal-content"
-					style={{
-						borderRadius: "30px",
-					}}
-				>
-					<div className="modal-header">
-						<h5 className="modal-title" id="exampleModalLabel">
-							{title}
-						</h5>
-						<button
-							type="button"
-							className="close"
-							data-dismiss="modal"
-							aria-label="Close"
-							onClick={() => setShow(false)}
-						>
-							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-					<div className="modal-body container-fluid">
-						<div
-							className="d-flex flex-column align-items-center px-2"
-							style={{
-								overflowY: "scroll",
-							}}
-						>
-							<div className="d-flex flex-row w-100">
-								<div
-									className={`d-flex flex-column justify-content-between px-2 ${
-										styles[`display${content.type}`]
-									}`}
-								>
-									<img
-										src={content.source}
-										className={styles[`displayConfig${content.type}`]}
-										alt={title}
-										loading="lazy"
-										title={title}
-									/>
-								</div>
-								<div className="d-flex flex-column px-2 justify-content-evenly">
-									<div className="d-flex flex-row align-items-center py-2">
-										<strong className=" pr-3">Type:</strong>
-										<div className="d-flex flex-column">
-											{content.type === "App" ? "Mobile App" : content.type}
-										</div>
-									</div>
-									<div className="d-flex flex-row align-items-center py-2">
-										<strong className=" pr-3">Year:</strong>
-										<div className="d-flex flex-column">{content.year}</div>
-									</div>
-									{content.url != "#" ? (
-										<div className="d-flex flex-row align-items-center py-2">
-											<strong className="text-left pr-3">View</strong>
-											<Link href={content.url}>
-												<div
-													className="d-flex flex-column text-left"
-													style={{
-														cursor: "pointer",
-														textDecoration: "underline",
-													}}
-												>
-													{content.url}
-												</div>
-											</Link>
-										</div>
-									) : (
-										<span className="text-left text-danger">
-											Preview Not Available
-										</span>
-									)}
-								</div>
-							</div>
-							<div className="flex-column mt-5">
-								<strong>Description 📋</strong>
-								{details.length &&
-									details?.map((detail: string) =>
-										detail != "" ? (
-											<div
-												key={Math.random()}
-												className="d-flex flex-row w-100 py-2 text-left"
-											>
-												{`🎯 ${detail}`}
-											</div>
-										) : (
-											""
-										)
-									)}
-							</div>
-						</div>
-					</div>
-					<div className="modal-footer">
-						<button
-							type="button"
-							className="btn btn-danger"
-							data-dismiss="modal"
-							onClick={() => setShow(false)}
-						>
-							Close
-						</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
-}
-export default Modal
+  return (
+    <div
+      className={`modal fade ${show ? "show" : ""}`}
+      id={title.replace(/\s/g, "")}
+      tabIndex={-1}
+      role="dialog"
+      data-backdrop={show}
+      data-show={show}
+      data-focus={show}
+      aria-labelledby={title}
+      aria-hidden={`${show}`}
+      style={{
+        display: show ? "block" : "none",
+        backdropFilter: show ? "brightness(0.5)" : "none",
+        transition: "1s",
+        overflow: "scroll",
+      }}
+    >
+      <div
+        className="modal-dialog modal-dialog-centered modal-lg"
+        role="document"
+      >
+        <div
+          className="modal-content"
+          style={{
+            borderRadius: "30px",
+          }}
+        >
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
+              {title}
+            </h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+              onClick={() => setShow(false)}
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body container-fluid">
+            <div
+              className="d-flex flex-column align-items-center px-2"
+              style={{
+                overflowY: "scroll",
+              }}
+            >
+              <div className="d-flex flex-row w-100">
+                <div
+                  className={`d-flex flex-column justify-content-between px-2 ${
+                    styles[`display${content.type}`]
+                  }`}
+                >
+                  <img
+                    src={content.source}
+                    className={styles[`displayConfig${content.type}`]}
+                    alt={title}
+                    loading="lazy"
+                    title={title}
+                  />
+                </div>
+                <div className="d-flex flex-column px-2 justify-content-evenly">
+                  <div className="d-flex flex-row align-items-center py-2">
+                    <strong className=" pr-3">Type:</strong>
+                    <div className="d-flex flex-column">
+                      {content.type === "App" ? "Mobile App" : content.type}
+                    </div>
+                  </div>
+                  <div className="d-flex flex-row align-items-center py-2">
+                    <strong className=" pr-3">Year:</strong>
+                    <div className="d-flex flex-column">{content.year}</div>
+                  </div>
+                  {content.url != "#" ? (
+                    <div className="d-flex flex-row align-items-center py-2">
+                      <strong className="text-left pr-3">View</strong>
+                      <a href={content.url} target="_blank">
+                        <div
+                          className="d-flex flex-column text-left"
+                          style={{
+                            cursor: "pointer",
+                            textDecoration: "underline",
+                          }}
+                        >
+                          {content.url}
+                        </div>
+                      </a>
+                    </div>
+                  ) : (
+                    <span className="text-left text-danger">
+                      Preview Not Available
+                    </span>
+                  )}
+                </div>
+              </div>
+              <div className="flex-column mt-5">
+                <strong>Description 📋</strong>
+                {details.length &&
+                  details?.map((detail: string) =>
+                    detail != "" ? (
+                      <div
+                        key={Math.random()}
+                        className="d-flex flex-row w-100 py-2 text-left"
+                      >
+                        {`🎯 ${detail}`}
+                      </div>
+                    ) : (
+                      ""
+                    )
+                  )}
+              </div>
+            </div>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-danger"
+              data-dismiss="modal"
+              onClick={() => setShow(false)}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Modal;
--- data/index.ts
+++ data/index.ts
@@ -1,254 +1,267 @@
-import { Data } from "../types"
+import { Data } from "../types";
 
 const data: Data[] = [
-	{
-		"Incomlend & LCLite": {
-			type: "WebApp",
-			source: "/projects/lclite.png",
-			year: 2022,
-			url: "https://marketplace.lclite.com",
-			details: `Incomlend and LCLite FinTech Platform tooling. Tasked with Software Architecture Rationalisation, developing the component library required to be used in both platforms. 
+  {
+    Incomlend: {
+      type: "WebApp",
+      source: "/projects/incomlend.png",
+      year: 2022,
+      url: "https://app.incomlend.com",
+      details: `FinTech Platform architecture, development & tooling. 
+			• Squad collaboration using Slack and management using JIRA. CI/CD, issue-tracking and code review through GitLab & GitHub.
+			• Architecture & Development of an entire platform which seamlessly integrates the trade financing experience of our clients with numerous departments within the company.
+			• Empowering every department on their day-to-day with precision abstraction, data processing, ML & AI, thereby optimizing turnarounds and eliminating manual processes.
+			• Development Stack: ReactJS TypeScript PWA | MUI, Formik, Storybook, NPM, RollupJS | ESLint | Postgres-Python.`,
+    },
+  },
+  {
+    LCLite: {
+      type: "WebApp",
+      source: "/projects/lclite.png",
+      year: 2022,
+      url: "https://marketplace.lclite.com",
+      details: `Web3 FinTech Platform tooling. Tasked with Software Architecture Rationalisation, developing the component library required to be used in both platforms. 
 			• Squad collaboration using Slack and management using JIRA.
 			• Engineered along with 2 other developers in a cross-functional team of 10, based in the parent company in Singapore.
 			• Created a tooling division dedicated to delivering isolated, versatile and highly performant React component package library, thereby reducing platform Front-End development time and effort and increasing platform FMP by approximately 30%.
 			• Development Stack: ReactJS TypeScript| Storybook and JSDoc | ESLint | Webpack and RollupJS | NPM and GitLab.`,
-		},
-	},
-	{
-		"TAMM Abu Dhabi": {
-			type: "WebApp",
-			source: "/projects/tamm.png",
-			year: 2021,
-			url: "https://www.tamm.abudhabi",
-			details: `Project: TAMM, an eGovernance service platform for the Abu Dhabi Government. Tasked with developing the user journey of Abu Dhabi Digital Authority (ADDA). 
+    },
+  },
+  {
+    "TAMM Abu Dhabi": {
+      type: "WebApp",
+      source: "/projects/tamm.png",
+      year: 2021,
+      url: "https://www.tamm.abudhabi",
+      details: `Project: TAMM, an eGovernance service platform for the Abu Dhabi Government. Tasked with developing the user journey of Abu Dhabi Digital Authority (ADDA). 
 			• Completely WFH; Collaboration using Slack, Workspace, JIRA, Gitlab, Confluence
 			• 4 Senior Full Stack Developers among other members of the scrum team for the targeted journey. 
 			• Development Stack: ReactJS TypeScript with Redux frontend | NodeJS with ExpressJS backend | Jest and enzyme for testing | Camunda for Business Logic.`,
-		},
-	},
-	{
-		"United International Piping": {
-			type: "Website",
-			source: "/projects/united-international-piping.png",
-			year: 2021,
-			url: "https://www.unitedintlpipinggroupuk.com/",
-			details: `* A NextJS project styled with SCSS.
+    },
+  },
+  {
+    "United International Piping": {
+      type: "Website",
+      source: "/projects/united-international-piping.png",
+      year: 2021,
+      url: "https://www.unitedintlpipinggroupuk.com/",
+      details: `* A NextJS project styled with SCSS.
 			* Most competitive design and responsiveness for Industrial Websites in the UK.
 			* Inspired by the latest trends.
 			
 			NextJS, React-Bootstrap, SCSS, Redux RTK`,
-		},
-	},
-	{
-		"International Oilfield Supplies": {
-			type: "Website",
-			source: "/projects/ios-oman.png",
-			year: 2021,
-			url: "https://www.iosgroupoman.com",
-			details: `* A NextJS project styled with SCSS.
+    },
+  },
+  {
+    "International Oilfield Supplies": {
+      type: "Website",
+      source: "/projects/ios-oman.png",
+      year: 2021,
+      url: "https://www.iosgroupoman.com",
+      details: `* A NextJS project styled with SCSS.
 			* Most competitive design and responsiveness for Industrial Websites in Oman.
 			* Inspired by the latest trends.
 			
 			NextJS, React-Bootstrap, SCSS, Redux RTK`,
-		},
-	},
-	{
-		"United Piping Solutions": {
-			type: "Website",
-			source: "/projects/ups.png",
-			year: 2021,
-			url: "https://www.united-piping-solutions.vercel.com/",
-			details: `* A NextJS project styled with SCSS.
+    },
+  },
+  {
+    "United Piping Solutions": {
+      type: "Website",
+      source: "/projects/ups.png",
+      year: 2021,
+      url: "https://www.united-piping-solutions.vercel.com/",
+      details: `* A NextJS project styled with SCSS.
 			* Most competitive design and responsiveness for Industrial Websites in UAE.
 			* Inspired by the latest trends in design like iOS 15.
 			
 			NextJS, React-Bootstrap, SCSS, Redux RTK`,
-		},
-	},
-	{
-		"MarketPlace UAE": {
-			type: "WebApp",
-			source: "/projects/marketplace.png",
-			year: 2021,
-			url: "#",
-			details: `* Responsive Trading Software with automated input collection routines, reminders, analysis & reports, salability & profitability analyzer, excel import and export.
+    },
+  },
+  {
+    "MarketPlace UAE": {
+      type: "WebApp",
+      source: "/projects/marketplace.png",
+      year: 2021,
+      url: "#",
+      details: `* Responsive Trading Software with automated input collection routines, reminders, analysis & reports, salability & profitability analyzer, excel import and export.
 			* Front-End: NextJS, React, TypeScript, SCSS, Bootstrap, Redux ToolKit (RTK)
 			* Back-End, APIs & Databases: NodeJS, MongoDB, Strapi, Redis, Firebase
 			* CI/CD: Github, Travis CI, Vercel, Heroku`,
-		},
-	},
-	{
-		"Gulf Motorcycles Web": {
-			type: "Website",
-			source: "/projects/gmr.png",
-			year: 2021,
-			url: "https://www.gulfmotorcycles.com",
-			details: `* A NextJS project styled with SCSS and backed by Firebase.
+    },
+  },
+  {
+    "Gulf Motorcycles Web": {
+      type: "Website",
+      source: "/projects/gmr.png",
+      year: 2021,
+      url: "https://www.gulfmotorcycles.com",
+      details: `* A NextJS project styled with SCSS and backed by Firebase.
 			* The primary objective was to drive the SEOs for the project.
 			* A unique project bringing the nation's one-of-a-kind fully digital workshop.
 			* NextJS, React-Bootstrap, SCSS, Firebase RTDB, Messaging, Authentication, Postman`,
-		},
-	},
-	{
-		"Gulf Motorcycles Admin Panel": {
-			type: "WebApp",
-			source: "/projects/gmradmin.png",
-			year: 2021,
-			url: "https://www.gulfmotorcycles.com/admin",
-			details: `* A PWA built with NextJS, styled with SCSS and backed by Firebase services.
+    },
+  },
+  {
+    "Gulf Motorcycles Admin Panel": {
+      type: "WebApp",
+      source: "/projects/gmradmin.png",
+      year: 2021,
+      url: "https://www.gulfmotorcycles.com/admin",
+      details: `* A PWA built with NextJS, styled with SCSS and backed by Firebase services.
 			* The admin panel was designed to work in harmony with the React Native Cross-Platform booking app of the project.
 			* The panel helps with booking, sales, accounting and analysis for the project.
 			* NextJS, React-Bootstrap, SCSS, Firebase RTDB, Messaging, Authentication, Postman`,
-		},
-	},
-	{
-		"Gulf Motorcycles Mobile ": {
-			type: "App",
-			source: "/projects/gmrapp.png",
-			year: 2021,
-			url: "https://www.gulfmotorcycles.com",
-			details: `Cross-Platform Mobile App:
+    },
+  },
+  {
+    "Gulf Motorcycles Mobile ": {
+      type: "App",
+      source: "/projects/gmrapp.png",
+      year: 2021,
+      url: "https://www.gulfmotorcycles.com",
+      details: `Cross-Platform Mobile App:
 			* Built with TypeScript React Native and backed by Firebase
 			* A booking app to create and view bookings and statuses of the services booked.
 			* The app works in harmony with the PWA admin panel built for the project.
 			* React-Native, Expo SDK, Firebase RTDB, Messaging, Authentication, Postman`,
-		},
-	},
-	{
-		"Sana'a Kayum ": {
-			type: "WebApp",
-			source: "/projects/sk.png",
-			year: 2020,
-			url: "https://www.sanaakayum.com",
-			details: `*An E-Commerce PWA using MERN and JAMstack.
+    },
+  },
+  {
+    "Sana'a Kayum ": {
+      type: "WebApp",
+      source: "/projects/sk.png",
+      year: 2020,
+      url: "https://www.sanaakayum.com",
+      details: `*An E-Commerce PWA using MERN and JAMstack.
 			* Development in NextJS, GatsbyJS and React-Razzle with GraphQL and Redux (thunk)
 			* MongoDB, NodeJS, NextJS, ExpressJS, ReactJS, Ant Design, React-Bootstrap, material-ui, Tailwind CSS, GatsbyJS, React-Razzle, GraphQL, Redux
 			`,
-		},
-	},
-	{
-		"Dot10 App ": {
-			type: "App",
-			source: "/projects/dtapp.png",
-			year: 2019,
-			url: "https://www.dot10.com",
-			details: `*A Project management tool initially made in Kotlin, merged with React Native
+    },
+  },
+  {
+    "Dot10 App ": {
+      type: "App",
+      source: "/projects/dtapp.png",
+      year: 2019,
+      url: "https://www.dot10.com",
+      details: `*A Project management tool initially made in Kotlin, merged with React Native
 			* Gives the list of clients, projects and overviews in a dashboard view.
 			* Keeps track of real time data allowing efficient allocation of resources
 			`,
-		},
-	},
-	{
-		"Ariana Nathan ": {
-			type: "Website",
-			source: "/projects/ariana.png",
-			year: 2019,
-			url: "#",
-			details: `* A portfolio website developed for the young talent Ariana
+    },
+  },
+  {
+    "Ariana Nathan ": {
+      type: "Website",
+      source: "/projects/ariana.png",
+      year: 2019,
+      url: "#",
+      details: `* A portfolio website developed for the young talent Ariana
 			* Development done in Wordpress PHP
 			`,
-		},
-	},
-	{
-		"Diplomat's Summit App ": {
-			type: "App",
-			source: "/projects/dsapp.png",
-			year: 2019,
-			url: "https://diplomatssummit.com",
-			details: `* An app to view and register the latest diplomat events in UAE
+    },
+  },
+  {
+    "Diplomat's Summit App ": {
+      type: "App",
+      source: "/projects/dsapp.png",
+      year: 2019,
+      url: "https://diplomatssummit.com",
+      details: `* An app to view and register the latest diplomat events in UAE
 			* Development using Kotlin
 			`,
-		},
-	},
-	{
-		"Diplomat's Summit": {
-			type: "Website",
-			source: "/projects/ds.png",
-			year: 2018,
-			url: "https://diplomatssummit.com",
-			details: `* A website to view and register for the latest diplomat events in UAE and to register for DS Membership.
+    },
+  },
+  {
+    "Diplomat's Summit": {
+      type: "Website",
+      source: "/projects/ds.png",
+      year: 2018,
+      url: "https://diplomatssummit.com",
+      details: `* A website to view and register for the latest diplomat events in UAE and to register for DS Membership.
 			* Registered members can view the latest investment opportunities worldwide.
 			* Development initially using Vanilla JS & WordPress PHP later transitioning to JavaScript ReactJS
 			`,
-		},
-	},
-	{
-		"The Label Sticks ": {
-			type: "Website",
-			source: "/projects/tls.png",
-			year: 2018,
-			url: "#",
-			details: `* A PR website focussing on differend brands and its ambassadors worldwide.
+    },
+  },
+  {
+    "The Label Sticks ": {
+      type: "Website",
+      source: "/projects/tls.png",
+      year: 2018,
+      url: "#",
+      details: `* A PR website focussing on differend brands and its ambassadors worldwide.
 			* Development initially using Vanilla JS & WordPress PHP later transitioning to JavaScript ReactJS
 			`,
-		},
-	},
-	{
-		"Julian's Luxury ": {
-			type: "Website",
-			source: "/projects/julian.png",
-			year: 2018,
-			url: "#",
-			details: `* A website providing luxury goods and customisation services worldwide.
+    },
+  },
+  {
+    "Julian's Luxury ": {
+      type: "Website",
+      source: "/projects/julian.png",
+      year: 2018,
+      url: "#",
+      details: `* A website providing luxury goods and customisation services worldwide.
 			* Development using Vanilla JS & WordPress PHP
 			`,
-		},
-	},
-	{
-		"BuyDoBuy Advertising ": {
-			type: "Website",
-			source: "/projects/bdb.png",
-			year: 2017,
-			url: "http://www.bdbme.com",
-			details: `* A website offering advertising services across UAE.
+    },
+  },
+  {
+    "BuyDoBuy Advertising ": {
+      type: "Website",
+      source: "/projects/bdb.png",
+      year: 2017,
+      url: "http://www.bdbme.com",
+      details: `* A website offering advertising services across UAE.
 			* Development using Vanilla JS
 			`,
-		},
-	},
-	{
-		"Design Lab DXB ": {
-			type: "Website",
-			source: "/projects/dldxb.png",
-			year: 2017,
-			url: "#",
-			details: `* A website offering architecture and design services across UAE.
+    },
+  },
+  {
+    "Design Lab DXB ": {
+      type: "Website",
+      source: "/projects/dldxb.png",
+      year: 2017,
+      url: "#",
+      details: `* A website offering architecture and design services across UAE.
 			* Development using Vanilla JS
 			`,
-		},
-	},
-	{
-		"Marina Hotel Apartments": {
-			type: "Website",
-			source: "/projects/marina.png",
-			year: 2017,
-			url: "https://www.marinahotelapt.com",
-			details: `* A hotel portfolio website with online booking functionalities
+    },
+  },
+  {
+    "Marina Hotel Apartments": {
+      type: "Website",
+      source: "/projects/marina.png",
+      year: 2017,
+      url: "https://www.marinahotelapt.com",
+      details: `* A hotel portfolio website with online booking functionalities
 			* Development using Vanilla JS
 			`,
-		},
-	},
-	{
-		"Mercedez-Benz (Gargash) ": {
-			type: "WebApp",
-			source: "/projects/gargash.png",
-			year: 2017,
-			url: "https://www.gargashonlineshop.com",
-			details: `* A bilingual ecommerce webapp for the prestigious Mercedez-Benz's lifestyle accessories.
+    },
+  },
+  {
+    "Mercedez-Benz (Gargash) ": {
+      type: "WebApp",
+      source: "/projects/gargash.png",
+      year: 2017,
+      url: "https://www.gargashonlineshop.com",
+      details: `* A bilingual ecommerce webapp for the prestigious Mercedez-Benz's lifestyle accessories.
 			* Development using Vanilla JS & WordPress PHP
 			`,
-		},
-	},
-	{
-		"Dot10 ": {
-			type: "Website",
-			source: "/projects/dot10.png",
-			year: 2017,
-			url: "https://www.dot10.ae",
-			details: `* A website offering advertising services, interior & fitout services across UAE.
+    },
+  },
+  {
+    "Dot10 ": {
+      type: "Website",
+      source: "/projects/dot10.png",
+      year: 2017,
+      url: "https://www.dot10.ae",
+      details: `* A website offering advertising services, interior & fitout services across UAE.
 			* Development using Vanilla JS
 			`,
-		},
-	},
-]
+    },
+  },
+];
 
-export default data
+export default data;
